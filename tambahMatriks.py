2405e3175872b82498f9e56c8e6dca5ef0ceaab4
fikarchamiiiim <fikarchamim@yahoo.com>
2020-12-21 10:07:00 +0700

    Initial Commit

---
+++ tambahMatriks.py
@@ -0,0 +1,22 @@
+import serial
+import io
+# import struct
+
+# values = (1,2,3,4,5)
+
+# string = b''
+
+# for i in values:
+#     string += struct.pack('!B',i)
+
+# print(string)
+
+# ser = serial.Serial()
+# ser.port = 'COM8'
+# ser.baudrate = 115200
+# ser.open()
+# b = str(1)
+
+# ser.write(b)
+
+print(str(5)+str(5))
